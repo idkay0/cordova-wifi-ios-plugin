360ad1a38c27b88be3a4f10defcaeb87eeee09b1
idkay0 <idkay0@gmail.com>
2023-01-30 10:34:11 +0900

    rename WIFI  in javascript file

--- www/cordova-wifi-ios-plugin.js
+++ www/cordova-wifi-ios-plugin.js
@@ -2,10 +2,10 @@ var exec = require('cordova/exec');
 
 var WifiPlugin = {
     coolMethod: function (arg0, success, error) {
-        cordova.exec(success, error, "WifiPlugin", "coolMethod", [arg0]);
+        cordova.exec(success, error, "WIFI", "coolMethod", [arg0]);
     },
     getWifiInfo: function (arg0, success, error) {
-        cordova.exec(success, error, "WifiPlugin", "getWifiInfo", [arg0]);
+        cordova.exec(success, error, "WIFI", "getWifiInfo", [arg0]);
     }
 };
 
